93de3823de1c0185b92d1b5bc0d7055a3870d56c
iclal-7 <iclalyazgu803@gmail.com>
2024-10-28 02:21:46 +0300

    Update QuadD.go

--- QuadD.go
+++ QuadD.go
@@ -2,8 +2,16 @@ package main
 
 import "fmt"
 
-func kenarmi(i, j, x, y int) bool {
-	return i == 1 || i == y || j == 1 || j == x
+func SatirYazdir(ilkKar, ortaKar, sonKar string, uzunluk int) {
+	fmt.Print(ilkKar)
+	for i := 0; i < uzunluk-2; i++ {
+		fmt.Print(ortaKar)
+	}
+	if uzunluk > 1 {
+		fmt.Print(sonKar)
+	}
+
+	fmt.Println() // \n ile aynı şeyi yapar, bir alt satıra geçer
 }
 
 func QuadD(x, y int) {
@@ -11,22 +19,16 @@ func QuadD(x, y int) {
 		return
 	}
 	for i := 1; i <= y; i++ {
-		for j := 1; j <= x; j++ {
-			if (i == 1 && j == 1) || (i == y && j == 1) { // sol üst köşe ve sol alt köşe
-				fmt.Print("A")
-			} else if (i == 1 && j == x) || (i == y && j == x) { // sağ üst köşe ve sağ alt köşe
-				fmt.Print("C")
-			} else if kenarmi(i, j, x, y) { //sağ, sol, üst, alt kenarlar
-				fmt.Print("B")
-			} else {
-				fmt.Print(" ") // kalan yerlere boşluk koyuyoruz
-			}
+		if i == 1 {
+			SatirYazdir("A", "B", "C", x)
+		} else if i > 1 && i < y {
+			SatirYazdir("B", " ", "B", x)
+		} else {
+			SatirYazdir("A", "B", "C", x)
 		}
-		fmt.Print("\n") // her satırın sonunda alt satıra geçmesi için
 	}
 }
 
 func main() {
 	QuadD(5, 3)
 }
-
